1b34fafc60421867e80671f90ce43901a861efed
Votre nom ou pseudo <rousset.estelle@gmail.com>
2017-04-21 10:08:19 +0200

    connexion

--- app/app.js
+++ app/app.js
@@ -4,6 +4,6 @@
 angular.module('components', []); 
 angular.module('services', []); 
 
-angular.module('app', ['components', 'services']); 
+angular.module('app', ['components', 'services','ngCookies']); 
 
 
