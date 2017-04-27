484babb0600614af48232f430c261b5dedccf8bd
Votre nom ou pseudo <rousset.estelle@gmail.com>
2017-04-27 11:37:30 +0200

    edit profil

--- js/components/main/main.js
+++ js/components/main/main.js
@@ -1,1 +1,26 @@
+'use strict';
 
+angular.module("components")
+    .component("main", {
+        templateUrl: "js/components/main/main.html",
+
+        bindings: {
+            reponses: '<',
+        },
+
+        controller: ['$cookies', '$window', function ($cookies, $window) {
+
+
+            this.$onInit = () => {
+                this.myId = $cookies.get('id');
+            };
+
+            this.deconnexion = () => {
+                $cookies.remove('id');
+                $cookies.remove('tokenSecure');
+                $window.location.href = '/#!/index';
+                this.myId = null;
+            }
+
+        }]
+    });
